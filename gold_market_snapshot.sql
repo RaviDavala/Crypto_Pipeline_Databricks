089f9592173dc4a1cb1033b9f0582442c4fdddb3
Ravi <143244586+RaviDavala@users.noreply.github.com>
2026-03-08 11:36:50 +0530

    Delete gold_market_snapshot.sql

--- gold_market_snapshot.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REFRESH MATERIALIZED VIEW p1_crypto.gold_layer.gold_market_snapshot
-AS
-SELECT
-    CURRENT_TIMESTAMP() AS snapshot_time,
-    SUM(market_cap) AS total_market_cap,
-    SUM(total_volume) AS total_volume_24h,
-    SUM(total_supply) AS total_supply
-FROM p1_crypto.gold_layer.gold_latest_market_snapshot;
